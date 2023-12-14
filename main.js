2e0f5aaed84c334ceb29a747b219765a240968e0
Kuibagit <29101894+Kuibagit@users.noreply.github.com>
2023-12-14 20:37:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const COOKIE = process.env.COOKIE;
 
 const checkCOOKIE = async () => {
     if (!COOKIE.length) {
-        console.error('不存在 COOKIE ，请重新检查');
+        console.error('[!] 不存在 COOKIE ，请重新检查');
         return false;
     }
 
     const pairs = COOKIE.split(/\s*;\s*/);
     for (const pairStr of pairs) {
         if (!pairStr.includes('=')) {
-            console.error(`存在不正确的 COOKIE ，请重新检查`);
+            console.error(`[!] 存在不正确的 COOKIE ，请重新检查`);
             return false;
         }
     }
@@ -23,29 +23,36 @@ const checkIn = async () => {
         method: 'post',
         url: `https://bbs.kanxue.com/user-signin.htm`,
         headers: {
-            'User-Agent': 'HD1910(Android/7.1.2) (pediy.UNICFBC0DD/1.0.5) Weex/0.26.0 720x1280',
+            'User-Agent': 'HD1910(Android/7.1.2) (pediy.UNICFBC0DD/1.0.7) Weex/0.26.0 720x1280',
             'Cookie': COOKIE,
             'Connection': 'keep-alive',
             'Accept': '*/*'
         }
     };
-    return axios(options).catch(error => {
+    return axios(options).then(response => {
+        // 请求成功时输出响应内容
+        console.log('[*] Response Data:', response.data);
+        const msg = response.data.message;
+        if (msg != '您今日已签到成功') {
+            console.log(`[+] 🎉🎉🎉已完成签到，获得${msg}雪币`);
+        }
+    })
+    .catch(error => {
+        // 请求失败时的错误处理逻辑
         if (error.response) {
             // 请求成功发出且服务器也响应了状态码，但状态代码超出了 2xx 的范围
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
+            console.error('Error Response:', error.response.data);
+            console.error('Status Code:', error.response.status);
+            console.error('Headers:', error.response.headers);
         } else if (error.request) {
             // 请求已经成功发起，但没有收到响应
-            // `error.request` 在浏览器中是 XMLHttpRequest 的实例，
-            // 而在node.js中是 http.ClientRequest 的实例
-            console.log(error.request);
+             console.error('No Response Received:', error.request);
         } else {
             // 发送请求时出了点问题
-            console.log('Error', error.message);
+            console.error('Request Error:', error.message);
         }
-        console.log(error.config);
-    })
+          console.error('Request Config:', error.config);
+    });
 };
 
 const sendMsg = async (msg, code) => {
@@ -77,13 +84,13 @@ const sendMsg = async (msg, code) => {
     }).catch((error) => {
         if (error.response) {
             // 请求成功发出且服务器也响应了状态码，但状态代码超出了 2xx 的范围
-            console.warn(`PUSHPLUS推送 请求失败，状态码：${error.response.status}`);
+            console.warn(`[+] PUSHPLUS推送 请求失败，状态码：${error.response.status}`);
         } else if (error.request) {
             // 请求已经成功发起，但没有收到响应
-            console.warn('PUSHPLUS推送 网络错误');
+            console.warn('[!] PUSHPLUS推送 网络错误');
         } else {
             // 发送请求时出了点问题
-            console.log('Axios Error', error.message);
+            console.log('[+] Axios Error', error.message);
         }
     });
 };
@@ -93,20 +100,20 @@ const start = async () => {
         const checkCOOKIE_result = await checkCOOKIE();
         if (!checkCOOKIE_result) return;
         else {
-            console.log('COOKIE检查通过');
+            console.log('[+] COOKIE检查通过');
         }
         const checkIn_result = await checkIn();
         const message = checkIn_result?.data?.message;
         const code = checkIn_result?.data?.code;
         if (code == 0 || message == '您今日已签到成功') {
-            if (code == 0) console.log('checkin成功');
+            if (code == 0) console.log('[+] 签到成功');
             else console.log(message);
             if (!PUSHPLUS.length) {
-                console.warn('不存在 PUSHPLUS ，请重新检查');
+                console.warn('[!] 不存在 PUSHPLUS ，停止推送消息');
             }
             else {
                 const pushResult = (await sendMsg(message, code))?.data?.msg;
-                console.log('PUSHPLUS 推送结果', pushResult);
+                console.log('[+] PUSHPLUS 推送结果', pushResult);
             }
         }
         else{
